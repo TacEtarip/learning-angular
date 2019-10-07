2c77c2f29d577c833d0e8f158fa886fb6391112c
Luis Huertas <hu3rtas@gmail.com>
2019-10-06 22:44:48 -0500

    Final Primera Parte

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'nw-app',
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 
 /* Solo hace display a el primer componente

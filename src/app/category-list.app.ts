d138641c20e2688bbc7b6f6a6df1d1bb4bc85976
Luis Huertas <hu3rtas@gmail.com>
2019-10-07 23:20:25 -0500

    p2

--- src/app/category-list.app.ts
+++ src/app/category-list.app.ts
@@ -4,15 +4,15 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'categoryList'
 })
 
-export class CategoryListPipe implements PipeTransform{
+export class CategoryListPipe implements PipeTransform {
   transform(mediaItems) {
     const categories = [];
     mediaItems.forEach(mediaItem => {
-     if (categories.indexOf(mediaItem.category) <= -1) {
-       categories.push(mediaItem.category);
+      if (categories.indexOf(mediaItem.category) <= -1) {
+        categories.push(mediaItem.category);
 
-     }
+      }
     });
     return categories.join(', ');
   }
- }
+}
